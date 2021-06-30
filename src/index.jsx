fb6ee939563adb770cf0b557be5def2ea5580f35
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-06-30 22:58:46 +0200

    Redux initialize

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Redux
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/reducers/post.reducer.jsx
@@ -0,0 +1,5 @@
+const initialState = { stateDeBase: "Hello" };
+
+export default function postReducer(state = initialState, action) {
+  return initialState;
+}
