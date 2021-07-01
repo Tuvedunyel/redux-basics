e5f32b0bd19d5667793d0d60a5db3bbd64b5321d
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-01 23:12:19 +0200

    User and post Working, begining the work on the CRUD

--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,7 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
 import { getPosts } from "./actions/post.action";
+import { getUser } from "./actions/user.action";
 
 const store = createStore(
   rootReducer,
@@ -17,6 +18,7 @@ const store = createStore(
 );
 
 store.dispatch(getPosts());
+store.dispatch(getUser());
 
 ReactDOM.render(
   <Provider store={store}>
