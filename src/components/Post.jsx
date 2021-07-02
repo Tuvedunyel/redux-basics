d9291d408ed3888be29eecfdc849c2dd4e43db72
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-02 10:13:20 +0200

    Starting the editing of posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Like from "./Like";
+import { isEmpty } from "./Utils";
+import edit from "./../../public/icons/edit.svg";
+import supprimer from "./../../public/icons/delete.svg";
+import { useState } from "react";
 
 const Post = ({ post }) => {
+  const [editToggle, setEditToggle] = useState(false);
+  const user = useSelector((state) => state.userReducer);
+
   return (
     <div className="post">
+      {!isEmpty(user[0]) && user[0].pseudo === post.author && (
+        <div className="edit-delete">
+          <img
+            src={edit}
+            alt="Editer"
+            onClick={() => {
+              setEditToggle(!editToggle);
+            }}
+          />
+          <img src={supprimer} alt="Supprimer" />
+        </div>
+      )}
       <h2>{post.title}</h2>
       <img
         src="https://picsum.photos/1500/400"
